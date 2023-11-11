5bc333bf88234516a3092f0cf1a5a6c73337b8db
muse918 <muse918911@gmail.com>
2023-11-11 21:46:02 +0900

    new_encode

---
+++ bot/src/evaluation/nnue_new.rs
@@ -0,0 +1,525 @@
+use std::{default, os, fs};
+
+use lazy_static::lazy_static;
+use libtetris::*;
+use serde::{
+    de::{Error, Visitor},
+    ser::SerializeSeq,
+    Deserialize, Serialize,
+};
+
+const STANDARD: Standard = Standard {
+    back_to_back: 52,
+    bumpiness: -24,
+    bumpiness_sq: -7,
+    row_transitions: -5,
+    height: -39,
+    top_half: -150,
+    top_quarter: -511,
+    jeopardy: -11,
+    cavity_cells: -173,
+    cavity_cells_sq: -3,
+    overhang_cells: -34,
+    overhang_cells_sq: -1,
+    covered_cells: -17,
+    covered_cells_sq: -1,
+    tslot: [8, 148, 192, 407],
+    well_depth: 57,
+    max_well_depth: 17,
+    well_column: [20, 23, 20, 50, 59, 21, 59, 10, -10, 24],
+
+    move_time: -3,
+    wasted_t: -152,
+    b2b_clear: 104,
+    clear1: -143,
+    clear2: -100,
+    clear3: -58,
+    clear4: 390,
+    tspin1: 121,
+    tspin2: 410,
+    tspin3: 602,
+    mini_tspin1: -158,
+    mini_tspin2: -93,
+    perfect_clear: 999,
+    combo_garbage: 150,
+
+    use_bag: true,
+    timed_jeopardy: true,
+    stack_pc_damage: false,
+    sub_name: None,
+};
+
+struct LayerVisitor<const I: usize, const O: usize>;
+impl<'de, const I: usize, const O: usize> Visitor<'de> for LayerVisitor<I, O> {
+    type Value = (Vec<[f32; O]>, [f32; O]);
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("an valid single precision floating point number")
+    }
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let mut weights = vec![[0.; O]; I];
+        let mut biases = [0.; O];
+        let mut encountered = 0;
+        for weight in weights.iter_mut().flatten() {
+            match seq.next_element()? {
+                Some(v) => *weight = v,
+                None => return Err(Error::invalid_length(encountered, &self)),
+            }
+            encountered += 1;
+        }
+        for bias in biases.iter_mut() {
+            match seq.next_element()? {
+                Some(v) => *bias = v,
+                None => return Err(Error::invalid_length(encountered, &self)),
+            }
+            encountered += 1;
+        }
+
+        Ok((weights, biases))
+    }
+}
+
+const ENCODE_LEN: usize = ((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + 7 + 1 + 20 + 7 * 5 + 7; // TODO: change later
+// const ENCODE_LEN: usize = 0;
+type VecT = f32;
+
+#[derive(Clone, Debug)]
+pub struct EncodeLayer<const O: usize, const MIN: i32, const MAX: i32> {
+    weights: Vec<[VecT; O]>,
+    biases: [VecT; O],
+}
+impl<const O: usize, const MIN: i32, const MAX: i32> Default for EncodeLayer<O, MIN, MAX> {
+    fn default() -> Self {
+        Self {
+            weights: vec![[0.; O]; ENCODE_LEN],
+            biases: [0.; O],
+        }
+    }
+}
+
+impl<const O: usize, const MIN: i32, const MAX: i32> EncodeLayer<O, MIN, MAX> {
+    // [0, 10 - M] x [0, 40 - N]
+    fn convolute<const M: usize, const N: usize>(field: &[[bool; 10]; 24]) -> [[i32; 10]; 24] {
+        let mut ret = field.map(|x| x.map(|y| y as i32));
+        for row in ret.iter_mut() {
+            for i in 0..=10 - M {
+                for j in 1..M {
+                    row[i] += row[i + j] << j;
+                }
+            }
+        }
+        for j in 0..10 {
+            for i in 0..=24 - N {
+                for k in 1..N {
+                    ret[i][j] += ret[i + k][j] << (M * k);
+                }
+            }
+        }
+        ret
+    }
+    fn forward(&self, board: &Board) -> [VecT; O] {
+        let mut ret = [0.; O];
+        let field = board.get_field();
+        let mut field_low = [[false; 10]; 24];
+        for i in 0..24 {
+            field_low[i] = field[i];
+        }
+
+        let conv_3x4 = Self::convolute::<3, 4>(&field_low);
+        for (i, v) in conv_3x4
+            .iter()
+            .take(25 - 4)
+            .flat_map(|x| x.iter().take(11 - 3))
+            .cloned()
+            .enumerate()
+        {
+            for j in 0..O {
+                ret[j] += self.weights[(i << 12) + v as usize][j];
+            }
+        }
+        let conv_10x1 = Self::convolute::<10, 1>(&field_low);
+        for (i, v) in conv_10x1
+            .iter()
+            .take(25 - 1)
+            .flat_map(|x| x.iter().take(11 - 10))
+            .cloned()
+            .enumerate()
+        {
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + (i << 10) + v as usize][j];
+            }
+        }
+        let conv_1x10 = Self::convolute::<1, 10>(&field_low);
+        for (i, v) in conv_1x10
+            .iter()
+            .take(25 - 10)
+            .flat_map(|x| x.iter().take(11 - 1))
+            .cloned()
+            .enumerate()
+        {
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + (i << 10) + v as usize][j];
+            }
+        }
+        //TO-DO: 큐, 기타 인코딩
+        for v in board.bag.iter().map(|x| match x {
+            Piece::I => 0,
+            Piece::O => 1,
+            Piece::T => 2,
+            Piece::L => 3,
+            Piece::J => 4,
+            Piece::S => 5,
+            Piece::Z => 6,
+        })
+        {
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + v as usize][j];
+            }
+        }
+        if board.b2b_bonus {
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + 7][j];
+            }
+        }
+        for j in 0..O {
+            ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + 7 + 1 + (board.combo).min(19) as usize][j]; 
+        }
+        for x in board.next_pieces.iter().take(5).enumerate() {
+            let idx = match x.1 {
+                Piece::I => 0,
+                Piece::O => 1,
+                Piece::T => 2,
+                Piece::L => 3,
+                Piece::J => 4,
+                Piece::S => 5,
+                Piece::Z => 6,
+            } + 7 * x.0;
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + 7 + 1 + 20 + idx][j]; 
+            }
+        }
+        if let Some(hold) = board.hold_piece {
+            let idx = match hold {
+                Piece::I => 0,
+                Piece::O => 1,
+                Piece::T => 2,
+                Piece::L => 3,
+                Piece::J => 4,
+                Piece::S => 5,
+                Piece::Z => 6,
+            };
+            for j in 0..O {
+                ret[j] += self.weights[((8 * 21) << 12) + ((1 * 24) << 10) + ((10 * 15) << 10) + 7 + 1 + 20 + 7 * 5 + idx][j]; 
+            }
+        }
+
+        for (v, bias) in ret.iter_mut().zip(self.biases) {
+            *v = (*v + bias).max(0.);
+        }
+
+        ret
+    }
+    fn with_random() -> Self {
+        let mut x = Self::default();
+        let mut state: i64 = 0x42;
+        for v in x.weights.iter_mut().flatten().chain(x.biases.iter_mut()) {
+            state ^= state.wrapping_shr(3);
+            state ^= state.wrapping_shl(3);
+            *v = (state as f32).abs().sqrt().sqrt().sqrt().sqrt();
+        }
+        x
+    }
+}
+
+impl<const O: usize, const MIN: i32, const MAX: i32> Serialize for EncodeLayer<O, MIN, MAX> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut seq = serializer.serialize_seq(Some(O * ENCODE_LEN + O))?;
+        for weight in self.weights.iter().flatten() {
+            seq.serialize_element(weight)?;
+        }
+        for bias in self.biases.iter() {
+            seq.serialize_element(bias)?;
+        }
+        seq.end()
+    }
+}
+
+impl<'de, const O: usize, const MIN: i32, const MAX: i32> Deserialize<'de>
+    for EncodeLayer<O, MIN, MAX>
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let v = deserializer.deserialize_seq(LayerVisitor::<ENCODE_LEN, O>)?;
+        Ok(EncodeLayer {
+            weights: v.0,
+            biases: v.1,
+        })
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct LinearClampLayer<const I: usize, const O: usize, const MIN: i32, const MAX: i32> {
+    weights: Vec<[VecT; O]>,
+    biases: [VecT; O],
+}
+
+impl<const I: usize, const O: usize, const MIN: i32, const MAX: i32> Serialize
+    for LinearClampLayer<I, O, MIN, MAX>
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut map = serializer.serialize_seq(Some(I * O + O))?;
+        for weight in self.weights.iter().flatten() {
+            map.serialize_element(weight)?;
+        }
+        for bias in self.biases {
+            map.serialize_element(&bias)?;
+        }
+        map.end()
+    }
+}
+
+impl<'de, const I: usize, const O: usize, const MIN: i32, const MAX: i32> Deserialize<'de>
+    for LinearClampLayer<I, O, MIN, MAX>
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let v = deserializer.deserialize_seq(LayerVisitor::<I, O>)?;
+        Ok(LinearClampLayer {
+            weights: v.0,
+            biases: v.1,
+        })
+    }
+}
+
+impl<const I: usize, const O: usize, const MIN: i32, const MAX: i32> Default
+    for LinearClampLayer<I, O, MIN, MAX>
+{
+    fn default() -> Self {
+        LinearClampLayer {
+            weights: vec![[0.; O]; I],
+            biases: [0.; O],
+        }
+    }
+}
+
+impl<const I: usize, const O: usize, const MIN: i32, const MAX: i32>
+    LinearClampLayer<I, O, MIN, MAX>
+{
+    fn forward(&self, inp: [VecT; I]) -> [VecT; O] {
+        let mut ret = [0.; O];
+        for (i, x) in inp.iter().enumerate() {
+            for (j, y) in self.weights[i].iter().enumerate() {
+                ret[j] += x * y;
+            }
+        }
+        for (x, bias) in ret.iter_mut().zip(self.biases) {
+            // *x = (*x + bias).clamp(MIN as VecT, MAX as VecT);
+            *x = (*x + bias).max(0.);
+        }
+        ret
+    }
+    fn forward_non_clamp(&self, inp: [VecT; I]) -> [VecT; O] {
+        let mut ret = [0.; O];
+        for (i, x) in inp.iter().enumerate() {
+            for (j, y) in self.weights[i].iter().enumerate() {
+                ret[j] += x * y;
+            }
+        }
+        for (x, bias) in ret.iter_mut().zip(self.biases) {
+            *x += bias
+        }
+        ret
+    }
+    fn with_random() -> Self {
+        let mut x = Self::default();
+        let mut state: i64 = 0x42;
+        for v in x.weights.iter_mut().flatten().chain(x.biases.iter_mut()) {
+            state ^= state.wrapping_shr(3);
+            state ^= state.wrapping_shl(3);
+            *v = (state as f32).abs().sqrt().sqrt().sqrt().sqrt();
+        }
+        x
+    }
+}
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+pub struct Nnue {
+    encode: EncodeLayer<128, 0, 1>, // encode & do first matmul
+    linear1: LinearClampLayer<128, 64, 0, 1>,
+    linear2: LinearClampLayer<64, 32, 0, 1>,
+    linear3: LinearClampLayer<32, 1, -1, 1>,
+}
+
+impl Nnue {
+    // (board, placement) values
+    fn forward(&self, board: &Board) -> f32 {
+        let l1 = self.encode.forward(board);
+        let l2 = self.linear1.forward(l1);
+        let l3 = self.linear2.forward(l2);
+        let l4 = self.linear3.forward_non_clamp(l3);
+        l4[0]
+    }
+    pub fn with_random() -> Self {
+        Self {
+            encode: EncodeLayer::with_random(),
+            linear1: LinearClampLayer::with_random(),
+            linear2: LinearClampLayer::with_random(),
+            linear3: LinearClampLayer::with_random(),
+        }
+    }
+}
+
+fn read_nnue(dir: String) -> Nnue {
+    let nnue_str = fs::read_to_string(dir).expect("failed to read nnue");
+    serde_json::from_str(&nnue_str).unwrap()
+}
+
+lazy_static! {
+    static ref MODEL: Nnue = read_nnue("D:\\muse918\\cold-clear-nnue-train\\src\\net\\net_new_encode.json".to_string());
+}
+
+use crate::evaluation::Evaluator;
+
+use super::{standard::{Reward, Value}, Standard};
+
+#[derive(Debug,Default,Serialize,Deserialize,Copy,Clone)]
+pub struct NnueEvaluator;
+
+impl Evaluator for NnueEvaluator {
+    type Value = Value;
+    type Reward = Reward;
+    fn name(&self) -> String {
+        "NNUE".to_string()
+    } 
+
+    fn pick_move(
+        &self,
+        candidates: Vec<crate::dag::MoveCandidate<Self::Value>>,
+        incoming: u32,
+    ) -> crate::dag::MoveCandidate<Self::Value> {
+        let mut backup = None;
+        for mv in candidates.into_iter() {
+            if incoming == 0
+                || mv.board.column_heights()[3..6]
+                    .iter()
+                    .all(|h| incoming as i32 - mv.lock.garbage_sent as i32 + h <= 20)
+            {
+                return mv;
+            }
+
+            match backup {
+                None => backup = Some(mv),
+                Some(c) if c.evaluation.spike < mv.evaluation.spike => backup = Some(mv),
+                _ => {}
+            }
+        }
+
+        return backup.unwrap();
+    }
+    fn evaluate(
+        &self,
+        lock: &LockResult,
+        board: &Board,
+        move_time: u32,
+        placed: Piece,
+    ) -> (Self::Value, Self::Reward) {
+        
+        let mut acc_eval = 0;
+
+        if lock.perfect_clear {
+            acc_eval += STANDARD.perfect_clear;
+        }
+        if STANDARD.stack_pc_damage || !lock.perfect_clear {
+            if lock.b2b {
+                acc_eval += STANDARD.b2b_clear;
+            }
+            if let Some(combo) = lock.combo {
+                let combo = combo.min(11) as usize;
+                acc_eval += STANDARD.combo_garbage * libtetris::COMBO_GARBAGE[combo] as i32;
+            }
+            match lock.placement_kind {
+                PlacementKind::Clear1 => {
+                    acc_eval += STANDARD.clear1;
+                }
+                PlacementKind::Clear2 => {
+                    acc_eval += STANDARD.clear2;
+                }
+                PlacementKind::Clear3 => {
+                    acc_eval += STANDARD.clear3;
+                }
+                PlacementKind::Clear4 => {
+                    acc_eval += STANDARD.clear4;
+                }
+                PlacementKind::Tspin1 => {
+                    acc_eval += STANDARD.tspin1;
+                }
+                PlacementKind::Tspin2 => {
+                    acc_eval += STANDARD.tspin2;
+                }
+                PlacementKind::Tspin3 => {
+                    acc_eval += STANDARD.tspin3;
+                }
+                PlacementKind::MiniTspin1 => {
+                    acc_eval += STANDARD.mini_tspin1;
+                }
+                PlacementKind::MiniTspin2 => {
+                    acc_eval += STANDARD.mini_tspin2;
+                }
+                _ => {}
+            }
+        }
+
+        if placed == Piece::T {
+            match lock.placement_kind {
+                PlacementKind::Tspin1 | PlacementKind::Tspin2 | PlacementKind::Tspin3 => {}
+                _ => acc_eval += STANDARD.wasted_t,
+            }
+        }
+
+        // magic approximation of line clear delay
+        let move_time = if lock.placement_kind.is_clear() {
+            (move_time / 10) as i32 + 40
+        } else {
+            (move_time / 10) as i32
+        };
+        acc_eval += STANDARD.move_time * move_time;
+
+
+        let highest_point = *board.column_heights().iter().max().unwrap() as i32;
+
+        acc_eval += STANDARD.jeopardy
+            * (highest_point - 10).max(0)
+            * if STANDARD.timed_jeopardy { move_time } else { 10 }
+            / 10;
+
+
+        let value = MODEL.forward(board);
+        (
+            Value {
+                value: (value) as i32,
+                spike: 0,
+            },
+            Reward {
+                value: acc_eval as i32,
+                attack: if lock.placement_kind.is_clear() {
+                    lock.garbage_sent as i32
+                } else {
+                    -1
+                },
+            },
+        )
+    }
+}
+
